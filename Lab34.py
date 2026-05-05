9686aa8a49bd5e68dbfa339aec2e0de8cdc7782c
tomy <tomy.bi@utp.ac.pa>
2026-05-04 19:47:58 -0500

    prueba

---
+++ Lab34.py
@@ -0,0 +1,5 @@
+a = """Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."""
+print(a)
+print("It´s alright")
+print("He is called 'Johnny'")
+print('He is called "johnny"')
